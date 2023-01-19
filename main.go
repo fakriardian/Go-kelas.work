87b94aee3fb070246cda00f64ea3bb69c21c479b
Irfan Fakri Ardian <mazbro9988@gmail.com>
2023-01-19 09:54:56 +0700

    struct circle

--- main.go
+++ main.go
@@ -1,36 +1,55 @@
 package main
 
-import "fmt"
-
+import (
+	"fmt"
+	"math"
+)
 
 type shape interface {
-    getArea() float64
-    getPerimeter() float64
+	getArea() float64
+	getPerimeter() float64
+}
+
+func measureShape(s shape) {
+	fmt.Printf("get area %v\n", s.getArea())
+	fmt.Printf("get perimeter %v\n", s.getPerimeter())
 }
 
 type rectangle struct {
-    width float64
-    height float64
+	width  float64
+	height float64
 }
 
-func (r rectangle) getArea() float64{
-    return r.width * r.height
+type circle struct {
+	radius float64
 }
 
-func (r rectangle) getPerimeter() float64{
-    return 2*r.width + 2*r.height
+func (r rectangle) getArea() float64 {
+	return r.width * r.height
 }
 
-func measureShape(s shape){
-    fmt.Printf("get area %v\n", s.getArea())
-    fmt.Printf("get perimeter %v\n", s.getPerimeter())
+func (r rectangle) getPerimeter() float64 {
+	return 2*r.width + 2*r.height
+}
+
+func (c circle) getArea() float64 {
+	return math.Phi * c.radius * c.radius
+}
+
+func (c circle) getPerimeter() float64 {
+	return 2 * math.Phi * c.radius
 }
 
 func main() {
-    myRectangle := rectangle{
-        width: 60,
-        height: 30,
-    }
+	myRectangle := rectangle{
+		width:  60,
+		height: 30,
+	}
 
-    measureShape(myRectangle)
+	myCircle := circle{
+		radius: 7,
+	}
+
+	measureShape(myRectangle)
+	measureShape(myCircle)
 }
